9b1ee1f2ab7a8027c61b0f2e9ec9c39ef6232b2a
yetianyes <yetianyes@qq.com>
2014-07-21 14:58:11 +0800

    code fix bug

--- ht/code.php
+++ ht/code.php
@@ -1,4 +1,5 @@
 <?php 
+error_reporting(0);
 require_once('conn.php');
 header("content-type:text/html;charset=utf-8"); 
 ini_set("magic_quotes_runtime",0); 
@@ -35,36 +36,33 @@ else {
 	@$check_exsist=mysql_query("select * from user where `email`= '$email1'");
 	@$check_exsist_row=mysql_fetch_row($check_exsist);
 	$dyh="'";
-		if (!$check_exsist_row)
-		{ 
+		if (!$check_exsist_row){ 
 			$result = mysql_query("INSERT INTO `evabioyetian`.`user` (`userid`, `email`, `pw`, `fm_name`, `addtime`, `fm_pic`, `name`, `dy_count`, `shenfen`) VALUES (NULL, '$email1', '$key1', '还没有想好封面名字', '$time', 'w.png', '还没有想好名字', '0', '0');");
 			$message='您不需要记住任何密码，需要登录时，点击进去即可。。。<br />请不要轻易泄露此链接，若想更换密码链接(24小时内，只能更换一次)，只需要重新输入邮箱即可。<br />www.zizhushu.com/ht/code_check.php?'.md5(email029).'='.$email2.'&'.md5(key029).'='.$key2.'&'.md5(url029).'='.$url2;
-			echo '0';
+			echo 0;
 					try { 
-					require('email_c.php');
+						require('email_c.php');
 					}
 					 catch (phpmailerException $e) { 
-					echo "邮件发送失败：".$e->errorMessage(); 
+						echo "邮件发送失败：".$e->errorMessage(); 
 					}
-		}
-		else {
+		}else {
 			  @$check_time=mysql_query("select `addtime` from user where `email`= '$email1' ORDER BY  `addtime` DESC LIMIT 0 ,1");
 			  @$check_time_row=mysql_fetch_row($check_time);
 			  $strtime=$check_time_row[0];
 			  $endtime=strftime( "%Y-%m-%d %H:%M:%S",strtotime($strtime)+60*60*24);
-			  if (strtotime($time)<strtotime($endtime))
-			  {
-				  echo '1';
+			  if (strtotime($time)<strtotime($endtime)){
+				 echo 1;
 			  }
 			  else {
 					$change_code=mysql_query("UPDATE  `evabioyetian`.`user` SET  `pw` =  '$key1',`addtime`='$time' WHERE  `user`.`email` ='$email1';");
 					$message='您不需要记住任何密码，需要登录时，点击进去即可。。。<br />请不要轻易泄露此链接，若想更换密码链接(24小时内，只能更换一次)，只需要重新输入邮箱即可。<br />www.zizhushu.com/ht/code_check.php?'.md5(email029).'='.$email2.'&'.md5(key029).'='.$key2.'&'.md5(url029).'='.$url2;
-					echo '0';
+					echo 0;
 						try { 
-						require('email_c.php');
+							require('email_c.php');
 						}
 						catch (phpmailerException $e) { 
-						echo "邮件发送失败：".$e->errorMessage(); 
+							echo "邮件发送失败：".$e->errorMessage(); 
 						}
 		      }
         }
--- muban/code_upload.php
+++ muban/code_upload.php
@@ -1,18 +1,19 @@
 var email1=$("#email").val();
 var url1=$("#url").val();
-if (!email1) {$("#spinfo").text("请输入密码。。。");setTimeout('$("#spinfo").text("")',2000);}
+if (!email1) {
+	$("#spinfo").text("请输入密码。。。");
+    setTimeout('$("#spinfo").text("")',2000);
+}
 else{
-$("#spinfo").text("登陆中。。。");
-$.post("ht/code.php", { email:email1, url:url1} ,function(data){
-if(data=='0')
-{
-$("#spinfo").text("亲，您的密码链接，已经发送到您的邮箱了，现在正在跳转到您的qq邮箱中。。。");
-setTimeout('window.location.href="http://mail.qq.com"',2000);
-}
-else
-{
-$("#spinfo").text("亲，您今天已经更改过密码了，若想更改密码的话，明天再来吧!");
-setTimeout('$("#spinfo").text("")',2000);
-}
-});
+	$("#spinfo").text("登陆中。。。");
+    $.post("ht/code.php", { email:email1, url:url1} ,function(data){
+        if(data==0){
+            $("#spinfo").text("亲，您的密码链接，已经发送到您的邮箱了，现在正在跳转到您的qq邮箱中。。。");
+            setTimeout('window.location.href="http://mail.qq.com"',2000);
+        }
+        else{
+            $("#spinfo").text("亲，您今天已经更改过密码了，若想更改密码的话，明天再来吧!");
+            setTimeout('$("#spinfo").text("")',2000);
+        }
+    });
 }
