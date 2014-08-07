0d8a622d115d9a7873f0c18a4fe60a4c5749950d
yetianyes <yetianyes@qq.com>
2014-08-07 09:37:02 +0800

    fix
    fix

--- 杂志导入工具/daoru.php
+++ 杂志导入工具/daoru.php
@@ -2,17 +2,16 @@
 $conn=mysql_connect("localhost","xxxx","xxxx");
 mysql_select_db("zizhushu",$conn);
 mysql_query("set names utf8"); 
-for ($i=0;$i<197;$i++)
-{
-$txt="zazhi/yilinzazhi/c (".$i.").txt";
-$content=file_get_contents($txt);
-$b=date("Y-m-d H:i:s");	
-$content1=preg_replace('/\n|\r\n/','',$content);	
-$result = mysql_query("INSERT INTO `zizhushu`.`wenzhang` (`wzid`, `userid`, `content`, `addtime`, `shenfen`, `pl_count`, `zl_count`, `zl_wzid`, `gm_count`) VALUES (NULL, '1', '$content1', '$b', '0', '0', '0', '0', '0');");
-sleep(1);
-$result1 = mysql_query("select wzid from wenzhang where userid = '1' ORDER BY  `wenzhang`.`addtime` DESC LIMIT 0 ,1");
-$row = mysql_fetch_row($result1);
-$result2 = mysql_query("UPDATE  `zizhushu`.`wenzhang` SET  `zl_wzid` =  $row[0] WHERE  `wenzhang`.`wzid` =$row[0];");
-echo 'ok<br />';
+for ($i=0;$i<197;$i++){
+	  $txt="zazhi/yilinzazhi/c (".$i.").txt";
+	  $content=file_get_contents($txt);
+	  $b=date("Y-m-d H:i:s");	
+	  $content1=preg_replace('/\n|\r\n/','',$content);	
+	  $result = mysql_query("INSERT INTO `zizhushu`.`wenzhang` (`wzid`, `userid`, `content`, `addtime`, `shenfen`, `pl_count`, `zl_count`, `zl_wzid`, `gm_count`) VALUES (NULL, '1', '$content1', '$b', '0', '0', '0', '0', '0');");
+	  sleep(1);
+	  $result1 = mysql_query("select wzid from wenzhang where userid = '1' ORDER BY  `wenzhang`.`addtime` DESC LIMIT 0 ,1");
+	  $row = mysql_fetch_row($result1);
+	  $result2 = mysql_query("UPDATE  `zizhushu`.`wenzhang` SET  `zl_wzid` =  $row[0] WHERE  `wenzhang`.`wzid` =$row[0];");
+	  echo 'ok<br />';
 }
 ?>
--- 杂志导入工具/daoru1.php
+++ 杂志导入工具/daoru1.php
@@ -3,8 +3,7 @@ $conn=mysql_connect("localhost","xxxx","xxxx");
 mysql_select_db("zizhushu",$conn);
 mysql_query("set names utf8"); 
 $result = mysql_query("select wzid from wenzhang where `zl_wzid`=0;");
-while ($result_row=mysql_fetch_row($result))
-{
-$result1 = mysql_query("UPDATE  `zizhushu`.`wenzhang` SET  `zl_wzid` ='$result_row[0]'   WHERE  `wenzhang`.`wzid` ='$result_row[0]';");
+while ($result_row=mysql_fetch_row($result)){
+		$result1 = mysql_query("UPDATE  `zizhushu`.`wenzhang` SET  `zl_wzid` ='$result_row[0]'   WHERE  `wenzhang`.`wzid` ='$result_row[0]';");
 }
 ?>
